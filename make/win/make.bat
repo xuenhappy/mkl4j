a772bc9a796a225c2c7c961d50f5ebdaab223271
xuen <xuen@mokahr.com>
2020-02-28 10:40:26 +0800

    add win dll

---
+++ make/win/make.bat
@@ -0,0 +1,5 @@
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+"C:\Program Files (x86)\IntelSWTools\parallel_studio_xe_2020.0.075\bin\psxevars.bat" intel64
+"C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\bin\mklvars.bat" intel64
+nmake /f Makefile.nmake
+
